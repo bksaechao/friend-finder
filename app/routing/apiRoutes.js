6e5ba8e6065d39557177be93112d79a572b9f15a
CaoKao <bksaechao@gmail.com>
2019-12-21 06:35:26 -0800

    created paths to respective html files in the htmlRoutes js file, and created a path to a parsed list of all friends

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,13 @@
+// Route to list of friends
+const allFriends = require("../data/friends")
+
+// Exporting to the server..
+// =============================================================
+module.exports = app => {
+    // Route to the JSON list of all friends
+    app.get("/api/friends", req, res => {
+        res.json(allFriends)
+    });
+
+    // POST route to handle survey results
+}
---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,15 @@
+const path = require("path");
+
+// Exporting to the server..
+// =============================================================
+module.exports = app => {
+    // Survey route
+    app.get("/survey", req, res => {
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
+    });
+
+    // Default, catch-all route to homepage
+    app.get("*", req, res => {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    })
+}
